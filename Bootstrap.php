2ffe00380f02822c4e947a8cb4d460fa533ff76a
Tim Kelty <tim@fusionary.com>
2013-06-18 13:53:30 -0400

    New EE version

--- Bootstrap.php
+++ Bootstrap.php
@@ -338,7 +338,7 @@ class Bootstrap
                 'site_label'                => 'ExpressionEngine 2 Boilerplate',
                 'cookie_expiration_in_days' => 30,
                 'min_php_version'           => '5.3.3',
-                'app_version'               => '260',
+                'app_version'               => '261',
                 'license_number'            => '',
                 'google_analytics_id'       => 'UA-XXXXXXX-XX',
                 'upload_preferences'        => array(),
