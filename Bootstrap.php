0f538bde079c6788704914542f57853a8db5a20c
Tim Kelty <tim@fusionary.com>
2013-07-30 08:32:50 -0400

    Adding Minimee path

--- Bootstrap.php
+++ Bootstrap.php
@@ -517,6 +517,7 @@ class Bootstrap
 
                 // Minimee
                 'minimee' => array(
+                    'base_path'  => $this->config['base_path'],
                     'cache_path'  => $this->config['public_cache_path'],
                     'cache_url'   => $this->config['public_cache_url'],
                     'minify_html' => 'yes',
